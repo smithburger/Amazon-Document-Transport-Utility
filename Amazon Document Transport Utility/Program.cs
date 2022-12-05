d17ac7fda2da8292912a5f0e5e25b18aaad891b7
Brian Smith <brian@broh.io>
2022-12-05 13:52:47 -0500

    Add project files.

---
+++ Amazon Document Transport Utility/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon_Document_Transport_Utility
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
